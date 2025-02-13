b5dfea3a60de2636ecb33ce982b8e6d9aed4ab08
ivangc09 <garcia.cruz.ivan09@gmail.com>
2025-02-13 16:14:52 -0600

    usamos api para clima de hoy y 5 dias posteriores

--- app.js
+++ app.js
@@ -1,7 +1,90 @@
 let cityInput = document.getElementById('city_input');
 let searchBtn = document.getElementById('searchBtn');
 let locationBtn = document.getElementById('locationBtn');
-let api_key = 'b827dd3423eb7efb584d8891fb6d8071';
+let currentWeatherCard = document.querySelectorAll('.weather-left .card')[0];
+let fiveDaysForecastCard = document.querySelector('.day-forecast');
+let apiKey = 'b827dd3423eb7efb584d8891fb6d8071';
+
+function getWeatherDetails(name,lat,lon,country,state){
+    let FORECAST_API_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`,
+    WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`,
+    days =[
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday'
+    ],
+
+    months = [
+        'Jan',
+        'Feb',
+        'Mar',
+        'Apr',
+        'May',
+        'Jun',
+        'Jul',
+        'Aug',
+        'Sep',
+        'Oct',
+        'Nov',
+        'Dec'
+    ]
+
+    fetch(WEATHER_API_URL).then(res => res.json()).then(data => {
+        let date = new Date();
+        currentWeatherCard.innerHTML = `
+            <div class="current-weather">
+                <div class="details">
+                    <p>Now</p>
+                    <h2>${(data.main.temp - 273.15).toFixed(2)}&deg;C</h2>
+                    <p>${data.weather[0].description}</p>
+                </div>
+                <div class="weather-icon">
+                        <img src="assets/04d@2x.png" alt="">
+                </div>
+            </div>
+            <hr>
+            <div class="card-footer">
+                <p><i class="fa-light fa-calendar"></i> &ensp; ${days[date.getDay()]}, ${date.getDate()}, ${months[date.getMonth()]} ${date.getFullYear()}</p>
+                <p><i class="fa-light fa-location-dot"></i> &ensp; ${name}, ${country}</p>
+            </div>        
+        `;
+    }).catch(() => {
+        alert('Failed to fetch the current weather');
+    });
+
+    fetch(FORECAST_API_URL).then(res => res.json()).then(data => {
+        let uniqueForecastDays = [];
+        let fiveDaysForecast = data.list.filter(forecast => {
+            let forecastDate = new Date(forecast.dt_txt).getDate();
+
+            if(!uniqueForecastDays.includes(forecastDate)){
+                return uniqueForecastDays.push(forecastDate);
+            }
+        });
+
+        fiveDaysForecastCard.innerHTML = '';
+        for(i = 0; i < fiveDaysForecast.length; i++){
+            let date = new Date(fiveDaysForecast[i].dt_txt);
+            fiveDaysForecastCard.innerHTML += `
+                <div class="forecast-item">
+                    <div class="icon-wrapper">
+                        <img src="assets/02d@2x.png" alt="">
+                        <span>${(fiveDaysForecast[i].main.temp - 273.15).toFixed(2)}&deg;C</span>
+                    </div>
+
+                    <p>${date.getDate()} ${months[date.getMonth()]}</p>
+                    <p>${days[date.getDay()]}</p>
+                </div>
+            `;
+        }
+    }).catch(() => {
+        alert('Failed to fetch weather forecast');
+    });
+}
 
 searchBtn.addEventListener('click',function getCityCoordinates (){
     let cityName = cityInput.value.trim();
@@ -11,11 +94,11 @@ searchBtn.addEventListener('click',function getCityCoordinates (){
         return;
     }
 
-    let GEOCODING_API_URL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName},{state code},{country code}&limit=1&
-    // appid=${api_key}`;
+    let GEOCODING_API_URL = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${apiKey}`
 
     fetch(GEOCODING_API_URL).then(res => res.json()).then(data => {
-        console.log(data)
+        let {name,lat,lon,country,state} = data[0];
+        getWeatherDetails(name,lat,lon,country,state)
     }).catch(() =>{
         alert(`Failed`);
     });
